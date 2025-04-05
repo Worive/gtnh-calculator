a63ee26f5c98015511f3cd14697e57f0c8ef83d3
safronov <Safronov.ag@gmail.com>
2025-04-06 02:38:05 +0300

    Test fix github actions & mini fixes

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,11 @@ import "./menu.js";
 import "./page.js";
 import "./recipeList.js";
 
+// Load the atlas image
+const atlas = new Image();
+atlas.src = "./data/atlas.webp";
+await new Promise((resolve) => {
+    atlas.onload = resolve;
+});
+
 document.getElementById("loading")?.remove();
