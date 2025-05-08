0a6b5a833aa3b14475edadb68a626a0fdce90275
safronov <Safronov.ag@gmail.com>
2025-05-08 15:44:25 +0300

    Reverted page import

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,10 @@ try {
         import("./tooltip.js"),
         import("./nei.js"),
         import("./menu.js"),
-        import("./recipeList.js"),
-        import("./page.js")
+        import("./recipeList.js")
     ]);
+    let page = await import("./page.js");
+    page.UpdateProject();
     loading.remove();
 } catch (error:any) {
     loading.innerHTML = "An error occurred on loading:<br>" + error.message;
