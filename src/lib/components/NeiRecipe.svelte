fe2024259f5321e2d9392e8201c5063d8ab3b842
Worive <13164341+Worive@users.noreply.github.com>
2025-05-11 13:54:00 +0200

    Recipe unused and legacy functions usages

--- src/lib/components/NeiRecipe.svelte
+++ src/lib/components/NeiRecipe.svelte
@@ -2,18 +2,16 @@
     import type {Recipe} from "$lib/core/data/models/Recipe";
     import {get} from "svelte/store";
     import {neiStore} from "$lib/stores/nei.store";
-    import {elementSize} from "$lib/types/constants/nei.consts";
     import NeiRecipeItemGrid from "$lib/components/nei/NeiRecipeItemGrid.svelte";
     import {RecipeIoType} from "$lib/types/enums/RecipeIoType";
     import type {RecipeInOut} from "$lib/types/models/Recipe";
+    import {voltageTier} from "$lib/types/constants/voltageTiers.const";
+    import {formatAmount} from "$lib/utils/Formatting";
 
     export let recipe: Recipe;
 
     const recipeType = recipe.recipeType;
     const builder = get(neiStore).mapRecipeTypeToRecipeList[recipeType.name];
-
-    const width = builder.CalculateWidth();
-    const height = builder.CalculateHeight(recipe);
 
     const showNeiCallback = get(neiStore).showNeiCallback;
     const canSelectRecipe = showNeiCallback?.onSelectRecipe != null;
@@ -23,7 +21,7 @@
     }
 </script>
 
-<div class="nei-recipe-box" style="left:0px; top:0px; width:{Math.round(width * elementSize)}px; height:${height * elementSize}px">
+<div class="nei-recipe-box">
     <div class="nei-recipe-io">
         <div class="nei-recipe-items">
             <NeiRecipeItemGrid
@@ -81,4 +79,11 @@
         {/if}
     {/if}
 </div>
-{@html builder.BuildRowDom([recipe], width, height, 0)}
+
+<style>
+    .nei-recipe-box {
+        left: 0;
+        top: 0;
+        padding: 5px;
+    }
+</style>
