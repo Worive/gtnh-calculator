36c80dfd223a9b637e63f9c207100ddf37d8c1c7
Worive <13164341+Worive@users.noreply.github.com>
2025-05-16 10:12:59 +0200

    Add renderScan to check updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <!-- src/routes/+layout.svelte -->
 <script lang="ts">
 	import GlobalCycleInitializer from '$lib/components/GlobalCycleInitializer.svelte';
+    import { dev } from '$app/environment';
+    import { RenderScan } from 'svelte-render-scan';
 </script>
+
+{#if dev}
+    <RenderScan initialEnabled={false}/>
+{/if}
 
 <GlobalCycleInitializer />
 
