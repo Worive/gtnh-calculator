6ba251d1722a72b78ae54ccd1304ac968e9c390d
Worive <13164341+Worive@users.noreply.github.com>
2025-05-18 12:36:10 +0200

    Implement production/consumption recipes

--- src/lib/components/nei/NeiRecipeTab.svelte
+++ src/lib/components/nei/NeiRecipeTab.svelte
@@ -1,18 +1,39 @@
 <script lang="ts">
-import {Goods} from "$lib/core/data/models/Goods.js";
-import {Recipe} from "$lib/core/data/models/Recipe.js";
-import NeiRecipe from "$lib/components/NeiRecipe.svelte";
-import {neiStore} from "$lib/stores/nei.store.js";
-import {repositoryStore} from "$lib/stores/repository.store.js";
-import {SearchQuery} from "$lib/core/data/models/SearchQuery.js";
+    import {Goods} from "$lib/core/data/models/Goods.js";
+    import {Recipe} from "$lib/core/data/models/Recipe.js";
+    import NeiRecipe from "$lib/components/NeiRecipe.svelte";
+    import {neiStore} from "$lib/stores/nei.store.js";
+    import {repositoryStore} from "$lib/stores/repository.store.js";
+    import {SearchQuery} from "$lib/core/data/models/SearchQuery.js";
+    import {ShowNeiMode} from "$lib/types/enums/ShowNeiMode";
 
-$: search = $neiStore.search;
+    $: search = $neiStore.search;
 
-$: recipes = ($neiStore.currentGoods instanceof Goods) ? Array.from($neiStore.currentGoods.production)
-    .map((pointer) => $repositoryStore?.GetObject(pointer, Recipe))
-    .filter((recipe) => recipe !== undefined)
-    .filter((recipe) => search ? recipe.MatchSearchText(new SearchQuery(search)) : true)
-    : [];
+    $: mode = $neiStore.currentMode;
+
+    $: recipes = getRecipes();
+
+    function getRecipes() {
+        if ($neiStore.currentGoods instanceof Goods) {
+
+            let goods: Int32Array;
+
+            if (mode === ShowNeiMode.Production) {
+                goods = $neiStore.currentGoods.production
+            } else if (mode === ShowNeiMode.Consumption) {
+                goods = $neiStore.currentGoods.consumption
+            } else {
+                throw new Error('Unknown NEI mode: ' + mode);
+            }
+
+            return Array.from(goods)
+                .map((pointer) => $repositoryStore?.GetObject(pointer, Recipe))
+                .filter((recipe) => recipe !== undefined)
+                .filter((recipe) => search ? recipe.MatchSearchText(new SearchQuery(search)) : true);
+        }
+
+        return [];
+    }
 
 </script>
 
@@ -26,7 +47,7 @@ $: recipes = ($neiStore.currentGoods instanceof Goods) ? Array.from($neiStore.cu
 
 <style>
     .recipe-list {
-        display:flex;
+        display: flex;
         max-height: 80vh;
         flex-wrap: wrap;
         overflow-y: scroll;
