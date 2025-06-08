3bf4fae90b9e241cc890cd76038020a87ff28a8f
Worive <13164341+Worive@users.noreply.github.com>
2025-06-08 11:56:38 +0200

    group: grouped-recipe - unify them back into a single file

--- src/lib/types/recipe/grouped-recipe.ts
+++
@@ -1,7 +0,0 @@
-import type { RecipeType } from '$lib/core/data/models/RecipeType';
-import { Recipe } from '$lib/core/data/models/Recipe';
-
-export interface GroupedRecipe {
-	type: RecipeType;
-	recipes: Recipe[];
-}
--- src/lib/types/recipe/grouped-recipes.ts
+++ src/lib/types/recipe/grouped-recipes.ts
@@ -1,5 +1,11 @@
-import type { GroupedRecipe } from '$lib/types/recipe/grouped-recipe';
+import type { RecipeType } from '$lib/core/data/models/RecipeType';
+import { Recipe } from '$lib/core/data/models/Recipe';
 
 export interface GroupedRecipes {
 	[key: string]: GroupedRecipe;
+}
+
+export interface GroupedRecipe {
+	type: RecipeType;
+	recipes: Recipe[];
 }
